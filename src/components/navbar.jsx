100f71ee1b01f7c780966625ee87b58e25f4aedd
studiosolmae <studiosolmae@gmail.com>
2025-10-24 08:19:38 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
       <nav className="navbar">
-        <img src="/logo.png" alt="Studio Solmae Logo" className="logo-img" style={{height: '80px'}}/>
+        <img src="public/logo.png" alt="Studio Solmae Logo" className="logo-img" style={{height: '80px'}}/>
         <ul className="nav-list">
           <li><a href="#about">About</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
